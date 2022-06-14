34e6938629f3d125668921cd94019b6db084b55f
thdailong <thdailong@gmail.com>
2022-06-14 12:12:41 +0700

    remove unnessary import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import Vue, { createApp } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import Antd from "ant-design-vue";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import "ant-design-vue/dist/antd.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
