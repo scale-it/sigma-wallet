fa51241bda386d1b402522feb4770d980c8517c7
thdailong <thdailong@gmail.com>
2022-06-15 15:24:54 +0700

    Remove unused file type and dependencies

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,12 +4,6 @@ import {
 	WebMode,
 } from "@algo-builder/web";
 
-export type StateValue = string | number | bigint;
-
-export type Key = string;
-
-export type DateRange = "start" | "end";
-
 export type WebModeTypes =
 	| WebMode
 	| MyAlgoWalletSession
