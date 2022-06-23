feb4967ebc1eb68fd809ca05b39bd3ccc5a4d650
thdailong <thdailong@gmail.com>
2022-06-23 19:49:23 +0700

    Test

--- src/pages/MsigUI.vue
+++ src/pages/MsigUI.vue
@@ -32,20 +32,42 @@
 	</a-row>
 </template>
 
-<script>
+<script lang="ts">
+import WalletStore from "@/store/WalletStore";
+import algosdk, {Transaction} from "algosdk";
+import str2arr from "../utility/StrToArray";
+
 export default {
 	name: "Multisignature UI",
 	data() {
+		const walletStore = WalletStore();
 		return {
 			unsignedJson: "",
 			signedJson: "",
 			buttonClass: "btn btn-danger",
 			formControl: "form-control",
+			walletStore,
+			Transaction,
 		};
 	},
 	methods: {
 		sign() {
-			this.signedJson = this.unsignedJson;
+			this.signedJson = this.walletStore.address;
+			let txn = str2arr(this.unsignedJson);
+			let haha = new Uint8Array(5);
+			haha[0] = 5;
+			console.log(txn);
+			console.log(haha);
+			//let Encodetxn = algosdk.encodeUnsignedTransaction(txn);
+			//let tmpTxt = new algosdk.Transaction(txn);
+			//console.log("Encoded Txn: " + Encodetxn);
+			//console.log("Transaction: " + tmpTxt);
+			// let decodeTxn = algosdk.decodeObj(Encodetxn);
+			// console.log("Decoded Txn: " + decodeTxn);
+			// //console.log("Transaction:" + txn);
+			// //console.log("tobyte: " + this.Transaction.toByte());
+			// let signedTxn = this.walletStore.webMode.signTransaction(decodeTxn);
+			// console.log("Signed Transaction: " + signedTxn);
 		},
 	},
 };
---
+++ src/utility/StrToArray.ts
@@ -0,0 +1,8 @@
+export default function str2arr(str: string) {
+    const buf = new ArrayBuffer(str.length * 1); // 2 bytes for each char
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
