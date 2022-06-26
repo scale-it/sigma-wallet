43e976dd4f7ef384c4a4a4bf6ec918cb1617582c
Megha-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-06-26 19:46:27 +0530

    tabs and txsender UI added

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+	<a-tabs v-model:activeKey="activeKey" type="card">
+		<a-tab-pane :key="Tabs.MultiSig" tab="MultiSig">
+			<msig />
+		</a-tab-pane>
+		<a-tab-pane :key="Tabs.TxSender" tab="Tx Sender">
+			<sender />
+		</a-tab-pane>
+	</a-tabs>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { Tabs } from "@/types";
+import Msig from "./MsigUI.vue";
+import Sender from "./TxSender.vue";
+
+export default defineComponent({
+	components: {
+		Msig,
+		Sender,
+	},
+	setup() {
+		return {
+			activeKey: ref(Tabs.Msig),
+			Tabs,
+		};
+	},
+});
+</script>
