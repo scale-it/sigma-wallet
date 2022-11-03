c3f1a0cc2dfc3a1cccc3589ee8e04a3d7179b1b0
MEGHA <100185149+Megha-Dev-19@users.noreply.github.com>
2022-11-03 22:49:50 +0530

    lint fixes

--- src/utilities/error.ts
+++ src/utilities/error.ts
@@ -1,13 +1,15 @@
-export function checkAddressPartOfMultisig(multisigAddress: any[], address: string) {
-    if (
-        Array.isArray(multisigAddress) &&
-        !multisigAddress.find(
-            (item: { address: string; signed: boolean }) =>
-                item.address === address
-        )
-    ) {
-        throw new Error(
-            "Connected account address is not part of the given multisig transaction."
-        )
-    }
+export function checkAddressPartOfMultisig(
+	multisigAddress: any[],
+	address: string
+) {
+	if (
+		Array.isArray(multisigAddress) &&
+		!multisigAddress.find(
+			(item: { address: string; signed: boolean }) => item.address === address
+		)
+	) {
+		throw new Error(
+			"Connected account address is not part of the given multisig transaction."
+		);
+	}
 }
